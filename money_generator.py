acef204a337f6b41e4ed60b97a288109dd2eac7d
Deyi Tan <deyixtan@gmail.com>
2022-01-15 02:26:07 +0800

    Update money generator script

--- money_generator.py
+++ money_generator.py
@@ -1,7 +1,12 @@
-value = input('How much would you like to generate?\n')
-print("Generating $$$...")
-try:
-    int(value)
-    print("You have generated $" + value)
-except:
-    print("Huh? Unable to generate that...")
+def generateMoney():
+    """Return money generated"""
+    value = input('How much would you like to generate?\n')
+    print("Generating $$$...")
+    try:
+        int(value)
+        print("You have generated $" + value)
+    except:
+        print("Huh? Unable to generate that...")
+
+if __name__ == "__main__":
+    generateMoney()
